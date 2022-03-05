3b9d6985a9f6bcbc287158135feee0ebbe06f0a2
sabdulaziz13 <abdulaziz13122002@gmail.com>
2022-03-05 20:51:12 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,16 +1,11 @@
-# from environs import Env
+from environs import Env
 
-# # environs kutubxonasidan foydalanish
-# env = Env()
-# env.read_env()
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
 
-# # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("ip")  # Xosting ip manzili
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str("ip")  # Xosting ip manzili
 
-import os
-
-BOT_TOKEN = str(os.environs.get("BOT_TOKEN"))
-ADMINS = list(os.environs.get("ADMINS"))
-IP = str(os.environs.get("ip"))
